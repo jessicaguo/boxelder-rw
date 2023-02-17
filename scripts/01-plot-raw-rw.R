f8b181a3c16160450e0d998589a669a30b5c6292
Jessica Guo <jessicaguo@email.arizona.edu>
2023-02-16 20:45:33 -0700

    Log-transform, then plot

--- scripts/01-plot-raw-rw.R
+++ scripts/01-plot-raw-rw.R
@@ -3,11 +3,21 @@ library(tidyverse)
 # Read in data
 rw <- read_csv("data_raw/3. post water off- rings H-K-2.csv")
 
+# non-transformed timeseries
 ggplot(rw, aes(x = Year, y = `Width (mm)`)) +
   geom_point(aes(col = SEX)) +
   geom_line(aes(col = SEX, group = ID))
 
-
+# non-transformed histograms
 ggplot(rw, aes(x = `Width (mm)`)) +
   geom_histogram(aes(fill = SEX)) +
   facet_wrap(~SEX)
+
+# log-transformed timeseries
+ggplot(rw, aes(x = Year, y = log(`Width (mm)`))) +
+  geom_point(aes(col = SEX)) +
+  geom_line(aes(col = SEX, group = ID))
+
+# log-transformed frequency polygons
+ggplot(rw, aes(x = log(`Width (mm)`))) +
+  geom_freqpoly(aes(col = SEX))
